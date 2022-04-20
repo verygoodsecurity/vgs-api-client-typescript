e715331b011e25a8c5da19786e4126ccc1c9c27c
Zinovii Dmytriv <zdmytriv@gmail.com>
2022-04-20 17:43:11 +0300

    update

--- scripts/run-tests-e2e.sh
+++ scripts/run-tests-e2e.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-LIB_VERSION=${LIB_VERSION:-0.0.1-alpha.$(date "+%Y%m%d%H%M")}
+export LIB_VERSION=${LIB_VERSION:-0.0.1-alpha.$(date "+%Y%m%d%H%M")}
 
 docker-compose build && \
 docker-compose run test
