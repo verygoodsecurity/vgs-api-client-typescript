62ed077f296ef14f8672d3f6403caca3905d38c6
Zinovii Dmytriv <zdmytriv@gmail.com>
2022-04-20 17:48:01 +0300

    update

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,8 @@ set -e
 usage() { echo "Required env var '$1' is missing"; exit 1; }
 
 [ -z "${ARTIFACT_REPOSITORY_TOKEN}" ] && usage "ARTIFACT_REPOSITORY_TOKEN" ;
-[ -z "${LIB_VERSION}" ] && usage "LIB_VERSION" ;
+
+LIB_VERSION=${LIB_VERSION:-0.0.1-alpha.$(date "+%Y%m%d%H%M")}
 
 docker-compose build && \
 docker-compose run assemble && \
